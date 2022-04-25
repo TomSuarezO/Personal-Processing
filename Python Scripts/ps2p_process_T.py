2119f68452bb0ef6c9da86da8a55b99ecfba1ea7
Tomas Suarez Omedas <tsuarezo@andrew.cmu.edu>
2022-04-25 14:07:45 -0400

    Update of two new ops sets and process code

--- Python Scripts/ps2p_process_T.py
+++ Python Scripts/ps2p_process_T.py
@@ -5,22 +5,26 @@ Created on Thu Jun 24 11:46:14 2021
 """
 
 # User-input parameters
-experimentName = r"bp200\20200207\pass\TSeries-02072020-1043-2417"
-rawDataServer = r"Z:\Christine\pvtdtomato" # server path for raw data - just before session ID folder
-saveServer = r"Z:\Tommy\Processed Data" # path where new folder will be created
+#mouseID = r"WT108"
+#expDate = r"033022"
+experimentName = r"PV2\042022"
+#experimentName = mouseID + "\\" expDate
+rawDataServer = r"W:\Data\2P_Testing\Nonspecific\2P" # server path for raw data - just before session ID folder
+saveServer = r"W:\Data\2P_Testing\Nonspecific\2P" # path where new folder will be created
 print('\nExperiment ID: ' + experimentName + '\nSaved in server:' + saveServer + '\n')
 
 use_custom_ops = True # Logical for default or custom options - Use True for options from file on next line
-opsFile = r"C:\Runyan Lab Codes and Data\Projects\Tommy-Personal-Processing\Ps2p Ops Files\RunyanLabMainOps_211130.npy" # Place full path and file name to options file - has to be in local drive
+opsFile = r"C:\Users\Williamson_Lab\Documents\Tommy\Ps2p Ops Files\WL_Ops_2Channel_042222.npy" # Place full path and file name to options file - has to be in local drive
 
 #------------------------------ DO NOT Edit Below this Line ------------------------------#
 #---------------------------(unless you know what you are doing)--------------------------#
 
 
-save_path = saveServer + '\\' + experimentName + r"_procPS2P"
 data_path = rawDataServer + '\\' + experimentName
+#save_path = data_path + mouseID + expDate + r"_procPS2P"
+save_path = saveServer + '\\' + experimentName + r"_procPS2P"
 
-print('\nSave Path: ' + save_path + '\nData Path:' + data_path + '\n')
+print('\nSave Path: ' + save_path + '\nData Path: ' + data_path + '\n')
 
 
 # Import Numpy for reading npy files and more
@@ -32,8 +36,12 @@ from suite2p import run_s2p, default_ops
 if use_custom_ops:
     ops0 = np.load(opsFile , allow_pickle=True)
     ops = ops0.tolist()
+    ops["tau"] = 1.25
+    ops["spatial_hp_reg"] = 42
+    #ops["nplanes"] = 1
+    #ops["nchannels"] = 2
     ops["save_folder"] = save_path
-    ops['input_format'] = 'bruker'
+    ops['input_format'] = 'tif'
     print('Custom Ops Loaded')
 else:
     ops = default_ops()
@@ -41,12 +49,14 @@ else:
 
 # Create Database
 db = {
-      'look_one_level_down': False, # whether to look in ALL subfolders when searching for tiffs
+      'look_one_level_down': True, # whether to look in ALL subfolders when searching for tiffs
       'data_path': [data_path], # a list of folders with tiffs
       #'tiff_list': ['suite2p_1_ctetSLC077_NC_210510_2P_plane1_1.tif']
     }
 
+print('############################## Beginning of Suite2p Processing ##############################')
 opsEnd = run_s2p(ops=ops, db=db)
+print('############################## End of Suite2p Processing ##############################')
 
 a = opsEnd
 import matplotlib.pyplot as plt
@@ -61,4 +71,4 @@ maxX = round(a['Lx']*a['maxregshift'])
 maxY = round(a['Ly']*a['maxregshift'])
 titlestr = 'Max Allowed X Shift = '+str(maxX) +'  |  '+'Max Allowed Y Shift = '+str(maxY)
 plt.title(titlestr)
-plt.savefig(save_path + '\\' + experimentName + '_MotionDiagnosis')
+plt.savefig(save_path + '\\' + 'MotionDiagnosis')
